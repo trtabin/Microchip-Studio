8c8d40745c84e225ddc9ef590be0fedad744649a
Tahmidur Rahman Tabin <trtabin@gmail.com>
2023-09-18 11:10:26 +0600

    Test 1: Blink LED

---
+++ Blink_LED/Blink_LED/main.c
@@ -0,0 +1,18 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+	DDRB |= (1<<DDB5);
+
+	while(1)
+	{
+		PORTB |= (1<<PORTB5);
+		_delay_ms(100);
+
+		PORTB &= ~(1<<PORTB5);
+		_delay_ms(100);
+	}
+}
